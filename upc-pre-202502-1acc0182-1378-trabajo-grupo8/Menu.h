6042c71bd1d08658a842d72c9b9f366e6c5532ff
AnaCamila <anacamila.patricio@gmail.com>
2025-10-04 22:40:16 -0500

    Añadi menu
    Primera version del menu

---
+++ upc-pre-202502-1acc0182-1378-trabajo-grupo8/Menu.h
@@ -0,0 +1,12 @@
+#pragma once
+#include<iostream>
+using namespace std;
+using namespace System;
+void MenuAdministrador() {
+	Console::Clear();
+	cout << "===Menu Administrador===\n" << endl;	
+
+}
+void MenuUsuario() {
+	cout << "Menu Usuario" << endl;	
+}

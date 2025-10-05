704fe977f1bd7f87d5645a678e2cdb4d313ca828
AnaCamila <anacamila.patricio@gmail.com>
2025-10-05 03:33:46 -0500

    Modificaciones en el menu y main
    Menu actualizado, ahora cuando se ejectuta te muestra el menu

--- upc-pre-202502-1acc0182-1378-trabajo-grupo8/Source.cpp
+++ upc-pre-202502-1acc0182-1378-trabajo-grupo8/Source.cpp
@@ -1,10 +1,19 @@
 #include <iostream>
 #include "Librerias.h"
+#include "Menu.h"
 using namespace std;
 
-int main()
-{
-	cout << "hello world"<<endl;
-	system("pause");
-	return 0;
+int main() {
+    int tipo;
+    cout << "Ingrese tipo de usuario (1 = Usuario, 2 = Admin): ";
+    cin >> tipo;
+
+    if (tipo == 1)
+        mostrarMenuUsuario();
+    else if (tipo == 2)
+        mostrarMenuAdministrador();
+    else
+        cout << "Tipo de usuario invalido.\n";
+
+    return 0;
 }

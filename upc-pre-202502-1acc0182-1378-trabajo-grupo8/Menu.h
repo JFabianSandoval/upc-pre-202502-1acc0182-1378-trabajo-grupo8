06a0adf2fc6f3d5a03f98c089d42432776ba1c3c
AnaCamila <anacamila.patricio@gmail.com>
2025-10-04 23:03:26 -0500

    Modificacion dek Menu,h
    Se le aplico lambda al menu

--- upc-pre-202502-1acc0182-1378-trabajo-grupo8/Menu.h
+++ upc-pre-202502-1acc0182-1378-trabajo-grupo8/Menu.h
@@ -1,12 +1,67 @@
 #pragma once
 #include<iostream>
+#include<functional>
+#include<map>
+
 using namespace std;
 using namespace System;
-void MenuAdministrador() {
-	Console::Clear();
-	cout << "===Menu Administrador===\n" << endl;	
 
-}
-void MenuUsuario() {
-	cout << "Menu Usuario" << endl;	
+int main() {
+    int tipo;
+    cout << "Ingrese tipo de usuario (1 = Usuario, 2 = Admin): ";
+    cin >> tipo;
+
+    if (tipo == 1) {
+        cout << "\n--- Menu Usuario ---\n";
+        cout << "1. Ver perfil\n";
+        cout << "2. Ver amigos\n";
+        cout << "3. Salir\n";
+
+        map<int, function<void()>> menuUsuario = {
+            {1, []() { cout << "Mostrando perfil del usuario...\n"; }},
+            {2, []() { cout << "Listando de amigos agregados...\n"; }},
+            {3, []() { cout << "Saliendo del menu de usuario...\n"; }}
+        };
+
+        int opcion;
+        do {
+            cout << "\nSeleccione una opcion: ";
+            cin >> opcion;
+
+            if (menuUsuario.count(opcion))
+                menuUsuario[opcion]();
+            else
+                cout << "Opcion invalida.\n";
+
+        } while (opcion != 3);
+    }
+    else if (tipo == 2) {
+        cout << "\n--- Menu Administrador ---\n";
+        cout << "1. Ver reportes\n";
+        cout << "2. Gestionar usuarios\n";
+        cout << "3. Salir\n";
+
+        map<int, function<void()>> menuAdmin = {
+            {1, []() { cout << "Mostrando reportes del sistema...\n"; }},
+            {2, []() { cout << "Gestionando usuarios...\n"; }},
+            {3, []() { cout << "Saliendo del menu de administrador...\n"; }}
+        };
+
+        int opcion;
+        do {
+            cout << "\nSeleccione una opcion: ";
+            cin >> opcion;
+
+            if (menuAdmin.count(opcion))
+                menuAdmin[opcion]();
+            else
+                cout << "Opcion invalida.\n";
+
+        } while (opcion != 3);
+    }
+    else {
+        cout << "Tipo de usuario invalido.\n";
+    }
+
+    return 0;
 }

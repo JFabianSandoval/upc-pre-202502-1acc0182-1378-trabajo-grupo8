7e56b3a8b499820f4f60b13a1dc17ff02dbd939f
Fabian Sandoval <103403098+JFabianSandoval@users.noreply.github.com>
2025-10-05 21:33:30 -0500

    correccion error anterior

--- upc-pre-202502-1acc0182-1378-trabajo-grupo8/Source.cpp
+++ upc-pre-202502-1acc0182-1378-trabajo-grupo8/Source.cpp
@@ -3,18 +3,13 @@
 #include "Menu.h"
 #include "Usuario.h"
 #include "Admin.h"
-#include "Windows.h"
 using namespace std;
-using namespace System;
 int main() {
     short tipo,op;
     while (true)
     {
 
     cout << "Ingrese tipo de usuario (1 = Usuario, 2 = Admin): ";
-    cout << "Credenciales de Usuario: pepito@upc.edu.pe | Contraseña: upc2020";
-
-
     cin >> tipo;
     string email, contrasenia;
     Usuario<string> usuario1;
@@ -25,7 +20,7 @@ int main() {
 
     if (tipo == 1)
     {
-        iniciarsesion(email,contrasenia);
+
         mostrarMenuUsuario(usuario1);
 
     }

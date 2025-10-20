2c6255de9d7c68020285820752e562b8826df462
JFabianSandoval <u20221a132@upc.edu.pe>
2025-10-20 18:58:02 -0500

    feat(web): agregar estructura base con main.cpp y CMakeLists

---
+++ web/main.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+#include <Wt/WApplication.h>
+#include <Wt/WContainerWidget.h>
+#include <Wt/WLineEdit.h>
+#include <Wt/WPushButton.h>
+#include <Wt/WText.h>
+#include <Wt/WBreak.h>
+
+
